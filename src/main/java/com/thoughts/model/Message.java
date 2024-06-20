9753d4dae134f58950ee7c0fa19ed1749062dbfd
Arseniy <sahalysyk02@mail.ru>
2024-06-20 22:26:50 +0500

    feat: create Message entity

---
+++ src/main/java/com/thoughts/model/Message.java
@@ -0,0 +1,24 @@
+package com.thoughts.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Message {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String text;
+
+    private String tag;
+}
