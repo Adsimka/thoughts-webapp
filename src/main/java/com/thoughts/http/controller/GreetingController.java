4acaa426fd4e0939e5999e63a114d6ca272772b4
Arseniy <sahalysyk02@mail.ru>
2024-06-20 22:10:32 +0500

    feat: add basic configuration for PostgreSQL connection in application.yaml

--- src/main/java/com/thoughts/http/controller/GreetingController.java
+++ src/main/java/com/thoughts/http/controller/GreetingController.java
@@ -13,7 +13,8 @@ public class GreetingController {
     @GetMapping
     public String greeting(
             @RequestParam(name="name", required=false, defaultValue="World") String name,
-            Model model) {
+            Model model
+    ) {
         model.addAttribute("name", name);
         return "greeting";
     }
