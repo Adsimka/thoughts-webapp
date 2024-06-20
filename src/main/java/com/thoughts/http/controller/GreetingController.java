4c46443f915372f1990eca66bf47a88ed2218592
Arseniy <sahalysyk02@mail.ru>
2024-06-20 22:50:35 +0500

    feat: create MessageService with findAll and create methods

--- src/main/java/com/thoughts/http/controller/GreetingController.java
+++ src/main/java/com/thoughts/http/controller/GreetingController.java
@@ -3,11 +3,9 @@ package com.thoughts.http.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@Controller
-@RequestMapping("/greeting")
+@Controller("/greeting")
 public class GreetingController {
 
     @GetMapping
---
+++ src/main/java/com/thoughts/service/MessageService.java
@@ -0,0 +1,2 @@
+package com.thoughts.service;public class MessageService {
+}
