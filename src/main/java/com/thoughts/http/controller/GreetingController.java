21a82053b1e5d0cb2d931ff1cf597c13cc729bc6
Arseniy <sahalysyk02@mail.ru>
2024-06-20 21:31:31 +0500

    feat: added greeting.html file to display the greeting A new greeting.html file has been created that displays the greeting message to users. The file will be used in the GreetingController controller

--- src/main/java/com/thoughts/http/controller/GreetingController.java
+++ src/main/java/com/thoughts/http/controller/GreetingController.java
@@ -1,4 +1,10 @@
 package com.thoughts.http.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/greeting")
 public class GreetingController {
+
 }
