3505fc1a32beef1939333472e96247c16d930c26
Arseniy <sahalysyk02@mail.ru>
2024-06-20 23:19:40 +0500

    feat: create MessageMapper which implementation Mapper

---
+++ src/main/java/com/thoughts/mapper/MessageMapper.java
@@ -0,0 +1,17 @@
+package com.thoughts.mapper;
+
+import com.thoughts.dto.MessageDto;
+import com.thoughts.model.Message;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageMapper implements Mapper<MessageDto, Message> {
+
+    @Override
+    public Message map(MessageDto object) {
+        return Message.builder()
+                .text(object.getText())
+                .tag(object.getTag())
+                .build();
+    }
+}
--- src/main/java/com/thoughts/model/Message.java
+++ src/main/java/com/thoughts/model/Message.java
@@ -5,12 +5,14 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @Entity
 public class Message {
 
