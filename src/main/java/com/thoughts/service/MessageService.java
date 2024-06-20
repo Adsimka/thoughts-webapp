23a83147b885a245fa8bca41a4ba740834009b00
Arseniy <sahalysyk02@mail.ru>
2024-06-20 23:34:35 +0500

    refactor: modify logic of create method in MessageService Add mapping logic from DTO to entity and save to database

--- src/main/java/com/thoughts/service/MessageService.java
+++ src/main/java/com/thoughts/service/MessageService.java
@@ -1,23 +1,30 @@
 package com.thoughts.service;
 
+import com.thoughts.dto.MessageDto;
+import com.thoughts.mapper.MessageMapper;
 import com.thoughts.model.Message;
 import com.thoughts.repository.MessageRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class MessageService {
 
     private final MessageRepository messageRepository;
+    private final MessageMapper messageMapper;
 
     public List<Message> findAll() {
         return messageRepository.findAll();
     }
 
-    public Message create(Message message) {
-        return messageRepository.save(message);
+    public Message create(MessageDto messageDto) {
+        return Optional.of(messageDto)
+                .map(dto -> messageMapper.map(dto))
+                .map(messageRepository::saveAndFlush)
+                .orElseThrow();
     }
 }
