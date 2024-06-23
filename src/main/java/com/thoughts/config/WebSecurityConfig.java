10b37a4abe0e249a2e5038b20fd1f2f07bbed50b
Arseniy <sahalysyk02@mail.ru>
2024-06-23 16:08:30 +0500

    feat: configure WebSecuritySpring for JDBC authentication in Spring Security

--- src/main/java/com/thoughts/config/WebSecurityConfig.java
+++ src/main/java/com/thoughts/config/WebSecurityConfig.java
@@ -1,19 +1,32 @@
 package com.thoughts.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.provisioning.JdbcUserDetailsManagerConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class WebSecurityConfig {
+
+    private final static String USERNAME_PASSWORD_ACTIVE_QUERY = """
+            SELECT username, password, active FROM users WHERE username = ?
+            """;
+
+    private final static String USERNAME_ROLES_JOIN = """
+            SELECT u.username, ur.roles FROM users u JOIN user_role ur ON u.id = ur.user_id WHERE u.username = ?
+            """;
+
+    private final DataSource dataSource;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -32,14 +45,17 @@ public class WebSecurityConfig {
     }
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-                User.withDefaultPasswordEncoder()
-                        .username("user")
-                        .password("password")
-                        .roles("USER")
-                        .build();
+    public JdbcUserDetailsManagerConfigurer<AuthenticationManagerBuilder> configure(AuthenticationManagerBuilder auth) {
+        try {
 
-        return new InMemoryUserDetailsManager(user);
+            return auth.jdbcAuthentication()
+                    .dataSource(dataSource)
+                    .passwordEncoder(NoOpPasswordEncoder.getInstance())
+                    .usersByUsernameQuery(USERNAME_PASSWORD_ACTIVE_QUERY)
+                    .authoritiesByUsernameQuery(USERNAME_ROLES_JOIN);
+
+        } catch (Exception exception) {
+            throw new RuntimeException("Error setting authentication parameters", exception);
+        }
     }
 }
--- src/main/java/com/thoughts/http/controller/GreetingController.java
+++ src/main/java/com/thoughts/http/controller/GreetingController.java
@@ -1,14 +1,13 @@
 package com.thoughts.http.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller("/")
 public class GreetingController {
 
     @GetMapping
-    public String greeting(Model model) {
+    public String greeting() {
         return "greeting";
     }
 }
