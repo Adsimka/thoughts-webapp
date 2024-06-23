785ec2666a8cba4aef01781dae44dcb854180454
Arseniy <sahalysyk02@mail.ru>
2024-06-23 16:51:50 +0500

    feat: create UserController

---
+++ src/main/java/com/thoughts/http/controller/UserController.java
@@ -0,0 +1,36 @@
+package com.thoughts.http.controller;
+
+import com.thoughts.model.User;
+import com.thoughts.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/registration")
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping
+    public String registration() {
+        return "auth/registration";
+    }
+
+    @PostMapping
+    public String create(Model model,
+                         @RequestParam User user) {
+        User userFromDb = userService.findByUsername(user.getUsername());
+        if (userFromDb != null) {
+            model.addAttribute("message", "User exists!");
+
+            return "registration";
+        }
+        return "redirect:/login";
+    }
+}
