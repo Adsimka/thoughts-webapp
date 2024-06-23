fb4b8e7c121c0e39974eea4bea4fb07bf0596da7
Arseniy <sahalysyk02@mail.ru>
2024-06-23 16:41:05 +0500

    feat: create UserRepository with method to find by username

---
+++ src/main/java/com/thoughts/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.thoughts.repository;
+
+import com.thoughts.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    User findByUsername(String username);
+}
