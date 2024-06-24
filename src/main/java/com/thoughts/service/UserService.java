c911c4a0fed189a182416efe7e5d70ac7752cc4a
Arseniy <sahalysyk02@mail.ru>
2024-06-24 22:41:11 +0500

    feat: implement UserDetailsService and override loadUserByUsername method

--- src/main/java/com/thoughts/service/UserService.java
+++ src/main/java/com/thoughts/service/UserService.java
@@ -3,11 +3,14 @@ package com.thoughts.service;
 import com.thoughts.model.User;
 import com.thoughts.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class UserService {
+public class UserService implements UserDetailsService {
 
     private final UserRepository userRepository;
 
@@ -17,5 +20,10 @@ public class UserService {
 
     public User save(User user) {
         return userRepository.save(user);
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username);
     }
 }
