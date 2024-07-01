86d34a455298bf9657f28e7085be4a32bb27b135
Arseniy <sahalysyk02@mail.ru>
2024-07-01 23:01:10 +0500

    feat: add user profile editing capability

--- src/main/java/com/thoughts/http/controller/UserController.java
+++ src/main/java/com/thoughts/http/controller/UserController.java
@@ -1,10 +1,13 @@
 package com.thoughts.http.controller;
 
+import com.thoughts.model.Role;
+import com.thoughts.model.User;
 import com.thoughts.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -19,5 +22,15 @@ public class UserController {
         model.addAttribute("users", userService.findAll());
         return "users/users";
     }
+
+    @GetMapping("{user}")
+    public String userEdit(Model model,
+                           @PathVariable User user) {
+        model.addAttribute("user", user);
+        model.addAttribute("roles", Role.values());
+
+        return "users/edit";
+    }
+
 }
 
