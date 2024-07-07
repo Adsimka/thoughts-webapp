e834cb3e002e9318981c24ad00973efb2bf1aad3
Arseniy <sahalysyk02@mail.ru>
2024-07-07 16:56:08 +0500

    feat: integrate Bootstrap via Thymeleaf fragments

--- src/main/java/com/thoughts/ThoughtsApplication.java
+++ src/main/java/com/thoughts/ThoughtsApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ThoughtsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ThoughtsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ThoughtsApplication.class, args);
+    }
 }
