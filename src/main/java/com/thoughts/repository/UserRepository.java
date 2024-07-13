dde08a2fff53be8288c5951f0929d305ea158b04
Arseniy <sahalysyk02@mail.ru>
2024-07-13 13:34:48 +0500

    add: add methods findByEmail and findByVerificationToken in UserRepository for email verification

--- src/main/java/com/thoughts/repository/UserRepository.java
+++ src/main/java/com/thoughts/repository/UserRepository.java
@@ -6,11 +6,16 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     User findByUsername(String username);
+
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByVerificationToken(String token);
 
     @Query("SELECT u FROM User u JOIN FETCH u.roles")
     List<User> findAllWithRoles();
